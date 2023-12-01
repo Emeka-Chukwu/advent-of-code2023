2b3ab1399280f9c25b14f1b561d17f018bf06b88
emeka-chukwu <emekapascal11@gmail.com>
2023-12-01 10:43:42 +0100

    first day

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("cali.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+	var total int64
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		row := scanner.Text()
+		numerals := extractStringNumbers(row)
+		val, _ := strconv.Atoi(numerals)
+		total += int64(val)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	fmt.Println(total)
+}
+
+func extractStringNumbers(input string) string {
+	reg := regexp.MustCompile("[0-9]+")
+	matches := reg.FindAllString(input, -1)
+	num := ""
+	for _, match := range matches {
+		num += match
+	}
+
+	return num
+}
